361e079bca27a7f36fe1fc3330661c7c2e8143bd
Jeremy Hull <sourdrums@gmail.com>
2012-11-27 01:28:16 -0700

    Add a simple map for experimenting with jumps

--- scripts/scenes/entry.js
+++ scripts/scenes/entry.js
@@ -5,13 +5,34 @@ function(Crafty) {
 
   Crafty.scene('entry', function() {
 
+    // Base floor
     Crafty.e('Platform')
-            .platform({ x: 0, y: 624, w: 1600, h: 16 }
+            .platform({ x: 0, y: 624, w: 3200, h: 16 }
                      , '#ffff00');
 
+    // first elevated platform
+    // * single jump
     Crafty.e('Platform')
             .platform({ x: 750, y: 570, w: 50, h: 16 }
                      , '#ffff00');
+
+    // Elevated floor
+    // * double jump
+    Crafty.e('Platform')
+            .platform({ x: 1050, y: 465, w: 700, h: 175 }
+                     , '#ffff00');
+
+    // Second elevated platform
+    // * long jump
+    Crafty.e('Platform')
+            .platform({ x: 2050, y: 465, w: 150, h: 16 }
+                     , '#ffff00');
+
+    // Small wall
+    Crafty.e('Platform')
+            .platform({ x: 0, y: 550, w: 16, h: 75 }
+                     , '#ffff00');
+
 
     Crafty.e('MainClone');
   });
