96267f547fc14c5e6feb335a3ad5b8d0f13133bc
Jeremy Hull <sourdrums@gmail.com>
2012-12-01 04:54:42 -0700

    Add more options for platform component

--- scripts/scenes/entry.js
+++ scripts/scenes/entry.js
@@ -7,32 +7,35 @@ function(Crafty) {
 
     // Base floor
     Crafty.e('Platform')
-            .platform({ x: 0, y: 624, w: 3200, h: 16 }
-                     , '#ffff00');
+            .platform({ x: 0, y: 624, w: 1600, h: 16 }
+                     , { color: '#ffff00' });
+
+    Crafty.e('Platform')
+            .platform({ x: 2050, y: 624, w: 1600, h: 16 }
+                     , { color: '#ffff00' });
 
     // first elevated platform
     // * single jump
     Crafty.e('Platform')
             .platform({ x: 750, y: 570, w: 50, h: 16 }
-                     , '#ffff00');
+                     , { color: '#ffff00' });
 
     // Elevated floor
     // * double jump
     Crafty.e('Platform')
             .platform({ x: 1050, y: 465, w: 700, h: 175 }
-                     , '#ffff00');
+                     , { color: '#ffff00' });
 
     // Second elevated platform
     // * long jump
     Crafty.e('Platform')
             .platform({ x: 2050, y: 465, w: 150, h: 16 }
-                     , '#ffff00');
+                     , { color: '#ffff00' });
 
     // Small wall
     Crafty.e('Platform')
             .platform({ x: 0, y: 550, w: 16, h: 75 }
-                     , '#ffff00');
-
+                     , { color: '#ffff00' });
 
     Crafty.e('MainClone');
   });
